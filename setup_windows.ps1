34bf549261d0604fd9372cac6efebc808de5c27a
Dasu Pradyumna <dasupradyumna@gmail.com>
2024-02-01 10:39:15 +0530

    feat(gitconfig): setup commit signing using SSH
      ~ updated `setup_windows.ps1`

--- setup_windows.ps1
+++ setup_windows.ps1
@@ -14,3 +14,16 @@ New-Item -Force -ItemType SymbolicLink `
 New-Item -Force -ItemType SymbolicLink `
     -Path "$env:USERPROFILE\.gitconfig" `
     -Target "$PWD\dotfiles\.gitconfig"
+
+# XXX: refactor below code
+
+# Uninstall and reinstall git to use external OpenSSH
+#
+# This ensures that Git does not ship with internal SSH support and uses the
+# SSH binaries configured externally by the PATH environment variable
+# If both external and internal SSH binaries exist, git commit signing fails
+# since it mixes up the SSH binaries used for signing
+# Refer to https://github.com/git-for-windows/git/issues/3647
+# and https://github.com/microsoft/winget-cli/discussions/3462
+winget uninstall Git.Git
+winget install --id=Git.Git --custom '/o:SSHOption=ExternalOpenSSH'
