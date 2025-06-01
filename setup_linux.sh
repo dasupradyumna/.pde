90dbb3a225af9bd1317bc530fdc4dde8f9b2c6b8
Dasu Pradyumna <dasupradyumna@gmail.com>
2025-06-02 03:55:12 +0530

    fix(setup): updated neovim linux appimage name
      ~ made program directory configurable
      - removed SHA256 hash sum check, since it is not straightforward now

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,191 +1,172 @@
+#!/bin/bash
 ############################################ LINUX SETUP ###########################################
 # Setup all programs required for the PDE in Linux and install their configs appropriately
 
 ##################### GLOBAL VARIABLES #####################
 
-# command-line options
+# Command-line options
 UPDATE=false
-NO_WEZTERM=false
+PROGRAM_DIR="$HOME/.local/bin"
+SKIP_WEZTERM=false
 UNINSTALL=false
-
-# relevant directories
-program_dir="$HOME/.local/bin"
-app_dir="$program_dir/app"
-
-# color terminal escape sequences
-c_RED='\033[0;31m'
-c_GREEN='\033[0;32m'
-c_CYAN='\033[0;36m'
-c_NONE='\033[m'
 
 ##################### UTILITY FUNCTIONS ####################
 
-# show help message and exit the script
-show-help() {
+# Show help message and exit the script
+show_help() {
     echo '
-Usage: bash setup_linux.sh [-h] [-u] [--no-wezterm] [--uninstall]
+Usage: bash setup_linux.sh [-h] [-u] [-d] [--skip-wez] [--uninstall]
     -h : show this help message
     -u : update the programs without re-installing the config files (default: OFF)
-    --no-wezterm : skip setting up wezterm (default: OFF)
+    -d : program directory to install binaries (default: ~/.local/bin)
+    --skip-wez : skip setting up wezterm (default: OFF)
     --uninstall : undo the filesystem changes made during setup'
-    exit $1 # exit with specified code
+    exit $1 # Exit with specified code
 }
 
-# write information
-write-info() { echo -en "$c_CYAN$1$c_NONE"; }
+# Write information
+write_info() { echo -en "\e[36m$1\e[m"; }
 
-# write an error message
-write-error() { echo -e "\n$c_RED! ERROR: $1$c_NONE"; }
+# Write an error message
+write_error() { echo -e "\n\e[31m! ERROR: $1\e[m"; }
 
 ###################### CORE FUNCTIONS ######################
 
-# downloads and extracts the nightly appimage from the given URL to the programs directory
-get-appimage-nightly() {
-    # parameters
-    local mini_url="$1"     # mini GitHub URL
+# Downloads and extracts the nightly appimage from the given URL to the programs directory
+get_appimage_nightly() {
+    local mini_url="$1"     # Mini GitHub URL
     local file="$2"         # AppImage file name
-    local sha256="$3"       # AppImage SHA256 file name
-    local bin="$4"          # name of symlink binary
+    local bin="$3"          # Name of symlink binary
 
-    write-info "Installing $mini_url -\n"
+    write_info "Installing $mini_url -\n"
     local file_url="https://github.com/$mini_url/releases/download/nightly/$file"
-    cd "$program_dir"
+    cd "$PROGRAM_DIR"
 
-    # download appimage file
-    write-info " * Downloading appimage from '$file_url' ... "
+    # Download appimage file
+    write_info " * Downloading appimage from '$file_url' ... "
     curl -L -o "$file" "$file_url" &>/dev/null
-    write-info 'Done\n'
+    write_info 'Done\n'
 
-    # verify the downloaded appimage integrity
-    write-info " * Verifying SHA hash from '$file_url.$sha256' ... "
-    local file_sha="$(curl -L "$file_url.$sha256" 2>/dev/null)"
-    [ "$file_sha" != "$(sha256sum "$file")" ] && {
-        write-error "'$mini_url' appimage SHA has did not match. Aborting installation!"
-        return 1
-    }
-    write-info 'Done\n'
-
-    # extract and install the appimage binaries
-    write-info " * Extracting '$file' and creating an executable symlink '$bin' ... "
-    rm -rf "$app_dir/$bin"
+    # Extract and install the appimage binaries
+    write_info " * Extracting '$file' and creating an executable symlink '$bin' ... "
+    rm -rf "$APP_DIR/$bin"
     chmod u+x "$file" && "./$file" --appimage-extract &>/dev/null
-    mv 'squashfs-root' "$app_dir/$bin"
+    mv 'squashfs-root' "$APP_DIR/$bin"
     rm -f "$file"
-    ln -sf "$app_dir/$bin/usr/bin/$bin" "$bin"
-    write-info 'Done\n'
+    ln -sf "$APP_DIR/$bin/usr/bin/$bin" "$bin"
+    write_info 'Done\n'
 
-    write-info 'Done.\n'
+    write_info 'Done.\n'
     cd - &>/dev/null
     return
 }
 
-# downloads and extracts an apt package to the programs directory
-apt-local-install() {
-    # parameters
+# Downloads and extracts an apt package to the programs directory
+apt_local_install() {
     local package="$1"      # APT package name
-    local binary="$2"       # package binary name
+    local binary="$2"       # Package binary name
 
-    write-info "Installing $package ... "
-    cd "$program_dir"
+    write_info "Installing $package ... "
+    cd "$PROGRAM_DIR"
     apt download "$package" &>/dev/null
     deb="$(find -type f -name "$package*.deb")"
     dpkg -x "$deb" "$package"
     mv "$package/usr/bin/$binary" .
     rm -rf "$package" "$deb"
     cd - &>/dev/null
-    write-info 'Done.\n'
+    write_info 'Done.\n'
 }
 
-# installs the configuration to the destination folder
-install-config() {
+# Installs the configuration to the destination folder
+install_config() {
     if [ $? -eq 1 ]; then
-        write-info 'Skipping config installation due to error in getting appimage\n'
+        write_info 'Skipping config installation due to error in getting appimage\n'
         return
     fi
     $UPDATE && return
 
-    # parameters
     local source="$1"
     local destination="$2"
 
-    write-info "Installed config from '$source' to '$destination'\n"
+    write_info "Installed config from '$source' to '$destination'\n"
     ln -sf "$source" "$destination"
 }
 
 ####################### MAIN PIPELINE ######################
 
-# parse the command-line arguments passed to it and issue relevant messages
-parse-arguments() {
-    # use getopt to parse arguments and show an error message if invalid arguments are found
-    local args="$(getopt --options h,u --longoptions no-wezterm,uninstall -- "$@" 2>./getopt_error)"
+# Parse the command-line arguments passed to it and issue relevant messages
+parse_arguments() {
+    # Use getopt to parse arguments and show an error message if invalid arguments are found
+    local args="$(getopt -o hud: -l skip-wez,uninstall -- "$@" 2>./getopt_error)"
     [ $? -ne 0 ] && error 'Failed to parse command-line arguments'
     local getopt_error_msg="$(cat ./getopt_error && rm ./getopt_error)"
-    [ -n "$getopt_error_msg" ] && { write-error "$getopt_error_msg" && show-help 1; }
+    [ -n "$getopt_error_msg" ] && { write_error "$getopt_error_msg" && show_help 1; }
 
-    # change the relevant globals based on the parsed arguments
+    # Change the relevant globals based on the parsed arguments
     eval "set -- $args"
-    for arg in $@; do
-        case "$arg" in
-            '-h') show-help 0 ;;
+    while [ -n "$1" ]; do
+        case "$1" in
+            '-h') show_help 0 ;;
             '-u') UPDATE=true ;;
-            '--no-wezterm') NO_WEZTERM=true ;;
+            '-d') PROGRAM_DIR="$2"; APP_DIR="$PROGRAM_DIR/appimages"; shift ;;
+            '--skip-wez') SKIP_WEZTERM=true ;;
             '--uninstall') UNINSTALL=true ;;
             '--') ;;
-            *) write-error 'setup_linux.sh does not take any positional arguments' && show-help 1 ;;
+            *) write_error 'setup_linux.sh does not take any positional arguments' && show_help 1 ;;
         esac
+        shift
     done
 }
 
-# main function that downloads and installs all programs
-install-pde() {
-    echo -en "${c_GREEN}Executing script for PDE setup on Linux\n\n$c_NONE"
+# Main function that downloads and installs all programs
+install_pde() {
+    echo -en "\e[32mExecuting script for PDE setup on Linux\n\n\e[m"
 
-    mkdir -p "$app_dir" "$HOME/.config" "$HOME/.ssh"
+    mkdir -p "$APP_DIR" "$HOME/.config" "$HOME/.ssh"
 
-    if ! $NO_WEZTERM; then
-        # install WezTerm (nightly) and its config
-        get-appimage-nightly 'wez/wezterm' 'WezTerm-nightly-Ubuntu20.04.AppImage' 'sha256' 'wezterm'
-        install-config "$PWD/wezterm" "$HOME/.config"
+    if ! $SKIP_WEZTERM; then
+        # Install WezTerm (nightly) and its config
+        get_appimage_nightly 'wez/wezterm' 'WezTerm-nightly-Ubuntu20.04.AppImage' 'wezterm'
+        install_config "$PWD/wezterm" "$HOME/.config"
         echo
     fi
 
-    # install Neovim (nightly) and its config
-    get-appimage-nightly 'neovim/neovim' 'nvim.appimage' 'sha256sum' 'nvim'
-    install-config "$PWD/nvim" "$HOME/.config"
+    # Install Neovim (nightly) and its config
+    get_appimage_nightly 'neovim/neovim' 'nvim-linux-x86_64.appimage' 'nvim'
+    install_config "$PWD/nvim" "$HOME/.config"
     echo
 
-    # install RipGrep locally
-    apt-local-install ripgrep rg
+    # Install RipGrep locally
+    apt_local_install ripgrep rg
     echo
 
-    # install all dotfiles
-    install-config "$PWD/dotfiles/.gitconfig" "$HOME"
-    install-config "$PWD/dotfiles/ssh_config" "$HOME/.ssh/config"
+    # Install all dotfiles
+    install_config "$PWD/dotfiles/.gitconfig" "$HOME"
+    install_config "$PWD/dotfiles/ssh_config" "$HOME/.ssh/config"
     [ -z "$BASH_INIT_SOURCED" ] && \
         (echo -e "\nsource $PWD/dotfiles/bash/init.sh" | tee -a "$HOME/.bashrc") >/dev/null
     ! $UPDATE && echo
 
-    echo -en "${c_GREEN}Completed PDE Setup\n$c_NONE"
+    echo -en "\e[32mCompleted PDE Setup\n\e[m"
 }
 
-uninstall-pde() {
-    echo -en "${c_GREEN}Executing script to uninstall PDE on Linux\n\n$c_NONE"
+uninstall_pde() {
+    echo -en "\e[32mExecuting script to uninstall PDE on Linux\n\n\e[m"
 
-    write-info "Removing programs from '$program_dir' and their configs from '$HOME/.config' ... "
-    rm -rf "$app_dir" "$program_dir/nvim" "$HOME/.config/nvim" "$program_dir/rg"
-    [ -L "$program_dir/wezterm" ] && rm -rf "$program_dir/wezterm" "$HOME/.config/wezterm"
-    write-info 'Done.\n'
+    write_info "Removing programs from '$PROGRAM_DIR' and their configs from '$HOME/.config' ... "
+    rm -rf "$APP_DIR" "$PROGRAM_DIR/nvim" "$HOME/.config/nvim" "$PROGRAM_DIR/rg"
+    [ -L "$PROGRAM_DIR/wezterm" ] && rm -rf "$PROGRAM_DIR/wezterm" "$HOME/.config/wezterm"
+    write_info 'Done.\n'
 
-    write-info 'Removing installed dotfiles ... '
+    write_info 'Removing installed dotfiles ... '
     rm -f "$HOME/.gitconfig" "$HOME/.ssh/config"
-    write-info 'Done.\n'
+    write_info 'Done.\n'
 
-    [ -n "$BASH_INIT_SOURCED" ] && \
-        write-info "NOTE: Please remove the following line manually from '$HOME/.bashrc' -
+    write_info "NOTE: Please remove the following line manually from '$HOME/.bashrc' -
         > source $PWD/dotfiles/bash/init.sh\n\n"
 
-    echo -en "${c_GREEN}Uninstalled PDE\n$c_NONE"
+    echo -en "\e[32mUninstalled PDE\n\e[m"
 }
 
-parse-arguments $@
-$UNINSTALL && uninstall-pde || install-pde
+parse_arguments $@
+$UNINSTALL && uninstall_pde || install_pde
