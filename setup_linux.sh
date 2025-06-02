d69ca3fe77f8ebf9f4d151314ca0c3deda2d2f75
Dasu Pradyumna <dasupradyumna@gmail.com>
2025-06-03 02:33:32 +0530

    feat(setup): improved linux script options
      ~ Neovim is now built from source, instead of an appimage
        This will be useful on systems with outdated dependencies (glibc)

--- setup_linux.sh
+++ setup_linux.sh
@@ -4,169 +4,213 @@
 
 ##################### GLOBAL VARIABLES #####################
 
-# Command-line options
+SYSTEM_INSTALL=false
 UPDATE=false
-PROGRAM_DIR="$HOME/.local/bin"
-SKIP_WEZTERM=false
 UNINSTALL=false
+SKIP_WEZTERM=false
+INSTALL_DIR=~/.local
+
+# Clean up Neovim source directory on exit
+set_exit_trap() { trap "tput cnorm; rm -rf $INSTALL_DIR/neovim" EXIT; }
 
 ##################### UTILITY FUNCTIONS ####################
 
 # Show help message and exit the script
 show_help() {
     echo '
-Usage: bash setup_linux.sh [-h] [-u] [-d] [--skip-wez] [--uninstall]
-    -h : show this help message
-    -u : update the programs without re-installing the config files (default: OFF)
-    -d : program directory to install binaries (default: ~/.local/bin)
-    --skip-wez : skip setting up wezterm (default: OFF)
-    --uninstall : undo the filesystem changes made during setup'
+Usage: ./setup_linux.sh [-hsuUW]
+    -h : Show this help message
+    -s : System install (/usr/local); if not set, fallback to user install (~/.local)
+    -u : Only update the programs without copying the configs
+    -U : Uninstall all programs and configs; if -s is set, uninstall from /usr/local
+    -W : Skip installing wezterm
+'
     exit $1 # Exit with specified code
 }
 
-# Write information
-write_info() { echo -en "\e[36m$1\e[m"; }
+log() {
+    local prefix=
+    local error_exit=false
+    OPTIND=1 # Reset before each getopts call
+    while getopts 'Eei' level; do
+        case "$level" in
+            E) prefix='\n\e[31mERROR'; error_exit=true ;; # Error and exit
+            e) prefix='\n\e[31mERROR' ;; # Error
+            i) prefix='\e[36mINFO' ;; # Information
+        esac
+        break
+    done
+    shift $((OPTIND - 1))
 
-# Write an error message
-write_error() { echo -e "\n\e[31m! ERROR: $1\e[m"; }
-
-###################### CORE FUNCTIONS ######################
-
-# Downloads and extracts the nightly appimage from the given URL to the programs directory
-get_appimage_nightly() {
-    local mini_url="$1"     # Mini GitHub URL
-    local file="$2"         # AppImage file name
-    local bin="$3"          # Name of symlink binary
-
-    write_info "Installing $mini_url -\n"
-    local file_url="https://github.com/$mini_url/releases/download/nightly/$file"
-    cd "$PROGRAM_DIR"
-
-    # Download appimage file
-    write_info " * Downloading appimage from '$file_url' ... "
-    curl -L -o "$file" "$file_url" &>/dev/null
-    write_info 'Done\n'
-
-    # Extract and install the appimage binaries
-    write_info " * Extracting '$file' and creating an executable symlink '$bin' ... "
-    rm -rf "$APP_DIR/$bin"
-    chmod u+x "$file" && "./$file" --appimage-extract &>/dev/null
-    mv 'squashfs-root' "$APP_DIR/$bin"
-    rm -f "$file"
-    ln -sf "$APP_DIR/$bin/usr/bin/$bin" "$bin"
-    write_info 'Done\n'
-
-    write_info 'Done.\n'
-    cd - &>/dev/null
-    return
+    echo -e "$prefix: $1\e[m"
+    if $error_exit; then exit 1; fi
 }
 
-# Downloads and extracts an apt package to the programs directory
-apt_local_install() {
-    local package="$1"      # APT package name
-    local binary="$2"       # Package binary name
+parse_opts() {
+    # Modify variables based on options
+    abspath() { echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"; }
+    while getopts ':hsuUW' option; do
+        case "$option" in
+            h) show_help 0 ;;
+            s) SYSTEM_INSTALL=true; INSTALL_DIR=/usr/local ;;
+            u) UPDATE=true ;;
+            U) UNINSTALL=true ;;
+            W) SKIP_WEZTERM=true ;;
+            \?) log -e "Invalid command-line option '-$OPTARG'!"; show_help 1 ;;
+            :) log -e "Command-line option '-$OPTARG' requires an argument!"; show_help 1 ;;
+        esac
+    done
+    unset -f abspath
+    shift $((OPTIND - 1))
+    set_exit_trap
 
-    write_info "Installing $package ... "
-    cd "$PROGRAM_DIR"
-    apt download "$package" &>/dev/null
-    deb="$(find -type f -name "$package*.deb")"
-    dpkg -x "$deb" "$package"
-    mv "$package/usr/bin/$binary" .
-    rm -rf "$package" "$deb"
-    cd - &>/dev/null
-    write_info 'Done.\n'
+    # Raise error in case of any positional arguments
+    if [ $# -ne 0 ]; then
+        log -e "Positional arguments are not supported! Received: $(printf "'%s' " $@)"
+        show_help 1
+    fi
+
+    log -i "Parsing command-line options ...
+    - SYSTEM_INSTALL: $SYSTEM_INSTALL
+    - UPDATE: $UPDATE
+    - UNINSTALL: $UNINSTALL
+    - SKIP_WEZTERM: $SKIP_WEZTERM"
 }
 
-# Installs the configuration to the destination folder
-install_config() {
-    if [ $? -eq 1 ]; then
-        write_info 'Skipping config installation due to error in getting appimage\n'
+############################ MAIN PROGRAM FUNCTIONS ############################
+
+install_wezterm() {
+    echo && log -i 'Installing WezTerm ...'
+    cd bin
+
+    # Download appimage
+    local appimage=WezTerm-nightly-Ubuntu20.04.AppImage
+    curl -L -o "$appimage" "https://github.com/wez/wezterm/releases/download/nightly/$appimage" || \
+        log -E "Failed to download appimage - $appimage"
+    log -i "Downloaded appimage - $appimage"
+
+    # Extract appimage
+    chmod u+x "$appimage" && "./$appimage" --appimage-extract 1>/dev/null || \
+        log -E "Failed to extract appimage - $appimage"
+    rm -rf "$appimage" .wezterm_appimage
+    mv squashfs-root .wezterm_appimage
+    log -i "Extracted appimage to '.wezterm_appimage' folder"
+
+    # Create symlink to appimage binary
+    ln -sf "$PWD/.wezterm_appimage/usr/bin/wezterm" wezterm || log -E 'Failed to create symlink'
+    log -i "Created symlink: $PWD/wezterm -> $PWD/.wezterm_appimage/usr/bin/wezterm"
+
+    cd ..
+}
+
+apt_install() {
+    local package="$1" binary="$2"
+
+    # System installation
+    if $SYSTEM_INSTALL; then
+        sudo apt-get install -y "$package" || log -E "Failed to install $package to system"
+        log -i "Successfully installed $package to system"
         return
     fi
+
+    # Local installation
+    cd bin
+    apt download "$package" &>/dev/null || log -E "Failed to download $package APT package"
+    local deb_file="$(find -type f -name "$package*.deb")"
+    dpkg -x "$deb_file" "$package" || log -E "Failed to extract $package APT package"
+    mv "$package/usr/bin/$binary" .
+    rm -rf "$package" "$deb_file"
+    log -i "Successfully installed $package to '$PWD'"
+    cd ..
+}
+
+install_neovim() {
+    echo && log -i 'Installing Neovim ...'
+
+    # Ensure Neovim dependencies are installed
+    command -v rg &>/dev/null || apt_install ripgrep rg
+    command -v xclip &>/dev/null || apt_install xclip xclip
+
+    # Clone Neovim repository
+    git clone --depth 1 https://github.com/neovim/neovim.git
+    log -i 'Cloned Neovim repository from GitHub'
+    cd neovim
+
+    # Build and install Neovim (nightly) from source
+    tput sc
+    {
+        make -j8 CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX="$INSTALL_DIR" 2>&1 || \
+            log -E 'Failed to build Neovim'
+
+        if $SYSTEM_INSTALL; then sudo make install; else make install; fi || \
+            log -E 'Failed to install Neovim'
+    } | while IFS= read -r line; do
+            local buffer=("${buffer[@]}" "$line")
+            [ ${#buffer[@]} -gt 10 ] && buffer=("${buffer[@]:1}")
+            # Below same as 'tput rc; tput ed'; `tput el` not needed because of newline
+            echo -e '\e[u\e[J'
+            for l in "${buffer[@]}"; do echo -e "\e[90m$l\e[m"; done
+            sleep 0.25
+        done || { echo && log -E 'Build-and-install process interrupted'; }
+    tput rc; tput ed
+    log -i "Successfully built Neovim (nightly) from source and installed to '$INSTALL_DIR'"
+
+    cd ..
+}
+
+# Copy a config file to its standard location via symlink
+copy_configs() {
     $UPDATE && return
+    local src="$1" dst="$2"
 
-    local source="$1"
-    local destination="$2"
-
-    write_info "Installed config from '$source' to '$destination'\n"
-    ln -sf "$source" "$destination"
+    ln -sf "$src" "$dst"
+    log -i "Copied config via symlink: $src -> $dst"
 }
 
-####################### MAIN PIPELINE ######################
-
-# Parse the command-line arguments passed to it and issue relevant messages
-parse_arguments() {
-    # Use getopt to parse arguments and show an error message if invalid arguments are found
-    local args="$(getopt -o hud: -l skip-wez,uninstall -- "$@" 2>./getopt_error)"
-    [ $? -ne 0 ] && error 'Failed to parse command-line arguments'
-    local getopt_error_msg="$(cat ./getopt_error && rm ./getopt_error)"
-    [ -n "$getopt_error_msg" ] && { write_error "$getopt_error_msg" && show_help 1; }
-
-    # Change the relevant globals based on the parsed arguments
-    eval "set -- $args"
-    while [ -n "$1" ]; do
-        case "$1" in
-            '-h') show_help 0 ;;
-            '-u') UPDATE=true ;;
-            '-d') PROGRAM_DIR="$2"; APP_DIR="$PROGRAM_DIR/appimages"; shift ;;
-            '--skip-wez') SKIP_WEZTERM=true ;;
-            '--uninstall') UNINSTALL=true ;;
-            '--') ;;
-            *) write_error 'setup_linux.sh does not take any positional arguments' && show_help 1 ;;
-        esac
-        shift
-    done
-}
-
-# Main function that downloads and installs all programs
 install_pde() {
-    echo -en "\e[32mExecuting script for PDE setup on Linux\n\n\e[m"
+    local start=$(date +%s)
 
-    mkdir -p "$APP_DIR" "$HOME/.config" "$HOME/.ssh"
+    # Ensure relevant directories exist
+    mkdir -p "$INSTALL_DIR" ~/.config ~/.ssh
 
-    if ! $SKIP_WEZTERM; then
-        # Install WezTerm (nightly) and its config
-        get_appimage_nightly 'wez/wezterm' 'WezTerm-nightly-Ubuntu20.04.AppImage' 'wezterm'
-        install_config "$PWD/wezterm" "$HOME/.config"
-        echo
-    fi
+    # Install programs
+    local pde_dir="$PWD"
+    pushd "$INSTALL_DIR" 1>/dev/null
+    if ! $SKIP_WEZTERM; then install_wezterm; copy_configs "$pde_dir/wezterm" ~/.config; fi
+    install_neovim; copy_configs "$pde_dir/nvim" ~/.config
+    popd 1>/dev/null
 
-    # Install Neovim (nightly) and its config
-    get_appimage_nightly 'neovim/neovim' 'nvim-linux-x86_64.appimage' 'nvim'
-    install_config "$PWD/nvim" "$HOME/.config"
-    echo
-
-    # Install RipGrep locally
-    apt_local_install ripgrep rg
-    echo
-
-    # Install all dotfiles
-    install_config "$PWD/dotfiles/.gitconfig" "$HOME"
-    install_config "$PWD/dotfiles/ssh_config" "$HOME/.ssh/config"
+    # Install dotfiles
+    echo && log -i 'Installing dotfiles ...'
+    copy_configs "$PWD/dotfiles/.gitconfig" ~
+    copy_configs "$PWD/dotfiles/ssh_config" ~/.ssh/config
     [ -z "$BASH_INIT_SOURCED" ] && \
-        (echo -e "\nsource $PWD/dotfiles/bash/init.sh" | tee -a "$HOME/.bashrc") >/dev/null
-    ! $UPDATE && echo
+        echo -e "\nsource $PWD/dotfiles/bash/init.sh" | tee -a ~/.bashrc 1>/dev/null
 
-    echo -en "\e[32mCompleted PDE Setup\n\e[m"
+    local end=$(date +%s)
+    local elapsed="$(date --utc --date "@$((end - start))" +%H:%M:%S)"
+    echo && log -i "PDE Setup time: $elapsed"
 }
 
 uninstall_pde() {
-    echo -en "\e[32mExecuting script to uninstall PDE on Linux\n\n\e[m"
+    # Remove programs
+    cd "$INSTALL_DIR"
+    [ -L 'bin/wezterm' ] && rm -rf bin/wezterm bin/.wezterm_appimage ~/.config/wezterm
+    rm -rf bin/nvim share/nvim/runtime ~/.config/nvim bin/rg bin/xclip
+    cd ~-
+    echo && log -i 'Removed all installed PDE programs'
 
-    write_info "Removing programs from '$PROGRAM_DIR' and their configs from '$HOME/.config' ... "
-    rm -rf "$APP_DIR" "$PROGRAM_DIR/nvim" "$HOME/.config/nvim" "$PROGRAM_DIR/rg"
-    [ -L "$PROGRAM_DIR/wezterm" ] && rm -rf "$PROGRAM_DIR/wezterm" "$HOME/.config/wezterm"
-    write_info 'Done.\n'
+    # Remove dotfiles
+    rm -f ~/.gitconfig ~/.ssh/config
+    log -i 'Removed all installed dotfiles'
 
-    write_info 'Removing installed dotfiles ... '
-    rm -f "$HOME/.gitconfig" "$HOME/.ssh/config"
-    write_info 'Done.\n'
+    log -i "NOTE: Please remove the following line manually from '~/.bashrc' -
+        > source $PWD/dotfiles/bash/init.sh"
 
-    write_info "NOTE: Please remove the following line manually from '$HOME/.bashrc' -
-        > source $PWD/dotfiles/bash/init.sh\n\n"
-
-    echo -en "\e[32mUninstalled PDE\n\e[m"
+    echo && log -i 'Uninstalled PDE'
 }
 
-parse_arguments $@
+parse_opts $@
+tput civis
 $UNINSTALL && uninstall_pde || install_pde
+tput cnorm
